50258b688dd83b95fdac4a399acf0bbd22121adf
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2014-04-11 12:29:30 -0700

    trying to do server thing and make images at the same time, because that makes sense

--- sierpinski.js
+++ sierpinski.js
@@ -1,9 +1,9 @@
 var gm = require("gm");
 
-gm("img.png")
-  .drawPoint(100, 200)
-  .write(dir + 'img.png', function(err){
-    if (err) return console.dir(arguments)
-    console.log(this.outname + ' created  :: ' + arguments[3])
-  }
-}
+gm("image.png").format(function(err, value){
+  console.log(value);
+})
+
+//gm("img.png")
+//  .drawPoint(100, 200)
+//  .write('out.png')
